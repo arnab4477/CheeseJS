25b3105ef9d2a186c022530710a5be1faa2a6a5d
arnab4477 <arnabsahareal@gmail.com>
2022-12-19 20:22:01 +0530

    Improved the generateCheckerBoard function (removed background-color property from the rows, used the square index to determine its color Added documentation)

--- src/utils/checkerboard.ts
+++ src/utils/checkerboard.ts
@@ -1,18 +1,17 @@
 import BoardArray from '../BoardArray';
 
-/*  
-This function generates a string of html that represents a checkered board with the specified light and dark square colors
-*/
+/**
+ * generateCheckeredBoard is a function that takes two required arguments:
+ * @param lightSquareColor string representing the color of the light squares on the chess board
+ * @param darkSquareColor string representing the color of the dark squares on the chess board
+ *
+ * and returns an HTML string representing a checkered board with the given colors.
+ */
+
 export const generateCheckeredBoard = (
   lightSquareColor: string,
   darkSquareColor: string
 ): string => {
-  // An array of the two colors that will be used to alternate between squares
-  const colors: string[] = [lightSquareColor, darkSquareColor];
-
-  // The index of the current color in the 'colors' array
-  let currentColorIndex: number = 1;
-
   // The current color that is being used
   let color: string;
 
@@ -21,32 +20,19 @@ export const generateCheckeredBoard = (
 
   // Loop through each row in the board
   for (let i = 0; i < BoardArray.length; i++) {
-    // Set the current color for the row
-    color = colors[currentColorIndex];
-
     // Add the opening tag for the row and set its background color to the current color
-    html += `<div class='row' style='background-color: ${color}'>`;
+    html += `<div class='row'>`;
 
     // Loop through each square in the row
     for (let j = 0; j < BoardArray.length; j++) {
-      // Update the current color index to the next color in the 'colors' array
-      currentColorIndex = (currentColorIndex + 1) % 2;
-
-      // Set the current color to the updated color index
-      color = colors[currentColorIndex];
+      // Determine the background color of the square
+      color = (i + j) % 2 === 0 ? lightSquareColor : darkSquareColor;
 
       // Add a div element for the square and set its background color to the current color
       html += `<div class='square' style='background-color: ${color}'></div>`;
     }
-
     // Add the closing tag for the row
     html += `</div>`;
-
-    // Update the current color index to the next color in the 'colors' array
-    currentColorIndex = (currentColorIndex + 1) % 2;
-
-    // Set the current color to the updated color index
-    color = colors[currentColorIndex];
   }
 
   // Return the html string
