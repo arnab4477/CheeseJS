8f1d03850cff49095e73e7091ddd57babb70bbcd
arnab4477 <arnabsahareal@gmail.com>
2022-12-20 21:12:46 +0530

    Refactored the `checker-board` component. Shortened the property names for the square colors. Generated and added the HTML after the initial component has loaded. Deleted the loggers because they were not working for some reason (some issues with the chalk library)

--- src/components/checker-board/checker-board.tsx
+++ src/components/checker-board/checker-board.tsx
@@ -9,19 +9,19 @@ import { generateCheckeredBoard } from '../../utils/checkerboard';
 })
 export class Checkerboard implements ComponentDidLoad {
   // Component properties for the square colors
-  @Prop({ mutable: true }) lightSquare: string = 'white';
-  @Prop({ mutable: true }) darkSquare: string = 'black';
+  @Prop({ mutable: true }) light?: string = 'white';
+  @Prop({ mutable: true }) dark?: string = 'black';
 
   // A reference to the checkerboard container element
   checkerboardContainer: HTMLElement;
 
-  // The html string for the checkered board, generated with the goven colors as props
-  checkerboardHTML = generateCheckeredBoard(this.lightSquare, this.darkSquare);
-
   // This method is called when the component has finished loading
   componentDidLoad() {
     // Set the inner html of the checkerboard container to the html string for the checkered board
-    this.checkerboardContainer.innerHTML = this.checkerboardHTML;
+    this.checkerboardContainer.innerHTML = generateCheckeredBoard(
+      this.light,
+      this.dark
+    );
   }
 
   render() {
--- src/utils/utils.ts
+++
@@ -1,4 +0,0 @@
-import chalk from 'chalk';
-
-export const log = (...values) => console.log(chalk.greenBright(...values));
-export const err = (...values) => console.log(chalk.redBright(...values));
