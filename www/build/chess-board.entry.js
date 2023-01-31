41230b84395fc5636fbf6c324136a55e29ccc73f
arnab4477 <arnabsahareal@gmail.com>
2023-02-01 04:26:59 +0530

    Fixed the issue which prevented the pieces from moving after click events in chess-board

--- www/build/chess-board.entry.js
+++ www/build/chess-board.entry.js
@@ -1,34 +1,23 @@
-import { Component, Prop, h, ComponentDidLoad } from '@stencil/core';
-import { generateChessBoard } from '../../utils/chessboard';
+import { r as registerInstance, h } from './index-725290d5.js';
+import { g as generateChessBoard } from './chessboard-456a9829.js';
+import './BoardTypes-d86232b4.js';
 
-@Component({
-  tag: 'chess-board',
-  styleUrl: 'chess-board.css',
-  shadow: true,
-})
-export class ChessBoard implements ComponentDidLoad {
-  // Component properties for the square colors and the FEN string
-  @Prop({ mutable: true }) light?: string = '#E0C35A';
-  @Prop({ mutable: true }) dark?: string = '#7A6A31';
-  @Prop({ mutable: true }) fen?: string =
-    'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
+const chessBoardCss = "#chess-board-container{position:relative;width:400px;height:400px;border:1px solid black}.row{display:flex;flex-direction:row;width:100%;height:50px}.square{width:50px;height:50px;border:0.1px black}.piece{display:flex;justify-content:center;align-items:center;touch-action:none;cursor:grab;cursor:move}.dragging{transform:scale(1.2);transition:0.2s ease-in-out}.invisible{display:none}@media (max-width: 550px){#chess-board-container{width:360px;height:360px}.row{height:45px}.square{width:45px;height:45px}.piece{height:40px}}";
 
-  // A reference to the checkerboard container element
-  chessBoardContainer: HTMLElement;
-
+const ChessBoard = class {
+  constructor(hostRef) {
+    registerInstance(this, hostRef);
+    this.light = '#E0C35A';
+    this.dark = '#7A6A31';
+    this.fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
+  }
   // This method is called when the component has finished loading
   componentDidLoad() {
     // Set the inner HTML of the checkerboard container to the HTML string for the checkered board
-    this.chessBoardContainer.innerHTML = generateChessBoard(
-      this.light,
-      this.dark,
-      this.fen
-    );
-
+    this.chessBoardContainer.innerHTML = generateChessBoard(this.light, this.dark, this.fen);
     // Get all the pieces and squares in the chess board
     const pieces = this.chessBoardContainer.querySelectorAll('.piece');
     const squares = this.chessBoardContainer.querySelectorAll('.square');
-
     // Add drag and drop event listeners to each piece
     pieces.forEach((piece) => {
       // When a piece is dragged, add the 'dragging' and 'invisible' classes to it
@@ -37,19 +26,16 @@ export class ChessBoard implements ComponentDidLoad {
           piece.classList.add('dragging', 'invisible');
         }, 0);
       });
-
       // When a piece is dropped, remove the 'dragging' and 'invisible' classes from it
       piece.addEventListener('dragend', () => {
         piece.classList.remove('dragging', 'invisible');
       });
-
       piece.addEventListener('click', () => {
         if (piece.classList.contains('dragging')) {
           piece.classList.remove('dragging');
           return;
         }
-        const otherHighlightedPiece =
-          this.chessBoardContainer.querySelector('.dragging');
+        const otherHighlightedPiece = this.chessBoardContainer.querySelector('.dragging');
         if (otherHighlightedPiece !== null) {
           const parentSquare = piece.parentElement;
           parentSquare.innerHTML = '';
@@ -60,7 +46,6 @@ export class ChessBoard implements ComponentDidLoad {
         piece.classList.add('dragging');
       });
     });
-
     // Add drag and drop event listeners to each square
     squares.forEach((square) => {
       // Allow dropping on the square by preventing the default behavior
@@ -70,19 +55,15 @@ export class ChessBoard implements ComponentDidLoad {
       // When a piece is dropped on a square, append the piece to the square and clear the square's inner HTML
       square.addEventListener('drop', (e) => {
         e.preventDefault();
-        const pieceBeingDragged =
-          this.chessBoardContainer.querySelector('.dragging');
-
+        const pieceBeingDragged = this.chessBoardContainer.querySelector('.dragging');
         square.innerHTML = '';
         square.appendChild(pieceBeingDragged);
       });
-
       square.addEventListener('click', (e) => {
         e.preventDefault();
-        const pieceBeingDragged =
-          this.chessBoardContainer.querySelector('.dragging');
-        if (pieceBeingDragged === null) return;
-
+        const pieceBeingDragged = this.chessBoardContainer.querySelector('.dragging');
+        if (pieceBeingDragged === null)
+          return;
         if (!(pieceBeingDragged.parentElement.id === square.id)) {
           pieceBeingDragged.classList.remove('dragging');
         }
@@ -91,13 +72,10 @@ export class ChessBoard implements ComponentDidLoad {
       });
     });
   }
-
   render() {
-    return (
-      <div
-        ref={(el) => (this.chessBoardContainer = el as HTMLElement)}
-        id="chess-board-container"
-      ></div>
-    );
+    return (h("div", { ref: (el) => (this.chessBoardContainer = el), id: "chess-board-container" }));
   }
-}
+};
+ChessBoard.style = chessBoardCss;
+
+export { ChessBoard as chess_board };
